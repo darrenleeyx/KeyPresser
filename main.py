ce8ba12c6290e69a7e57ab36249a7fb5b059e2fb
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-11-06 01:09:28 +0800

    bug fixes and refactor

--- main.py
+++ main.py
@@ -9,20 +9,6 @@ import fasteners
 import os
 import sys
 
-def cleanup():
-    try:
-        ui_manager.stop_event_listener_thread()
-        if event_listener_thread.is_alive():
-            event_listener_thread.join()
-        key_manager.stop_key_press()
-        key_manager.release_keys()
-    except Exception as e:
-        logger_manager.log_error(e)
-    finally:
-        lock.release()
-        root.destroy()
-
-
 lock_file_path = os.path.join(os.path.expanduser("~"), ".key_presser.lock")
 lock = fasteners.InterProcessLock(lock_file_path)
 if not lock.acquire(blocking=False):
@@ -30,20 +16,34 @@ if not lock.acquire(blocking=False):
     sys.exit(1)
 
 try:
+    root = tk.Tk()
     logger_manager = LoggerManager()
 
     if __name__ == "__main__":
         try:
-            root = tk.Tk()
-            key_manager = KeyManager()
+            key_manager = KeyManager(logger_manager)
             state_manager = StateManager(key_manager, logger_manager)
             ui_manager = UiManager(root, state_manager)
-            event_listener_thread = Thread(target=ui_manager.wait_for_state_changed_event, args=(state_manager.state_changed_event,))
-            event_listener_thread.start()
-            root.protocol("WM_DELETE_WINDOW", cleanup)
+            state_changed_event_polling_thread = Thread(target=ui_manager.state_changed_event_handler, args=(state_manager.state_changed_event,))
+            state_error_event_polling_thread = Thread(target=ui_manager.state_error_event_handler, args=(state_manager.state_error_event,))
+            state_changed_event_polling_thread.start()
+            state_error_event_polling_thread.start()
 
             root.mainloop()
+
+            ui_manager.stop_event_listener_thread()
+            if state_changed_event_polling_thread is not None and state_changed_event_polling_thread.is_alive():
+                state_changed_event_polling_thread.join()
+            if state_error_event_polling_thread is not None and state_error_event_polling_thread.is_alive():
+                state_error_event_polling_thread.join()
+            key_manager.stop_key_press()
+            key_manager.release_pressed_keys()
+
         except Exception as e:
             logger_manager.log_error(e)
+        finally:
+            if root.winfo_exists():
+                root.destroy()
 finally:
     lock.release()
+    sys.exit(0)
