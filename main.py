755acce3ccc6fe4e9e1d13f25c297659c697be30
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-11-05 23:36:26 +0800

    ensure that cleanup is performed when user clicks X

--- main.py
+++ main.py
@@ -9,10 +9,22 @@ import fasteners
 import os
 import sys
 
+def cleanup():
+    try:
+        ui_manager.stop_event_listener_thread()
+        if event_listener_thread.is_alive():
+            event_listener_thread.join()
+        key_manager.stop_key_press()
+        key_manager.release_keys()
+    except Exception as e:
+        logger_manager.log_error(e)
+    finally:
+        lock.release()
+        root.destroy()
+
+
 lock_file_path = os.path.join(os.path.expanduser("~"), ".key_presser.lock")
-
 lock = fasteners.InterProcessLock(lock_file_path)
-
 if not lock.acquire(blocking=False):
     messagebox.showerror("Error", "Another instance of this application is already running.")
     sys.exit(1)
@@ -22,23 +34,15 @@ try:
 
     if __name__ == "__main__":
         try:
-            # Starting
             root = tk.Tk()
             key_manager = KeyManager()
             state_manager = StateManager(key_manager, logger_manager)
             ui_manager = UiManager(root, state_manager)
             event_listener_thread = Thread(target=ui_manager.wait_for_state_changed_event, args=(state_manager.state_changed_event,))
             event_listener_thread.start()
-            
-            # Started
+            root.protocol("WM_DELETE_WINDOW", cleanup)
+
             root.mainloop()
-            
-            # Closing
-            ui_manager.stop_event_listener_thread()
-            if event_listener_thread.is_alive():
-                event_listener_thread.join()
-            key_manager.stop_key_press()
-            key_manager.release_keys()
         except Exception as e:
             logger_manager.log_error(e)
 finally:
