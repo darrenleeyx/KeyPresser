7d3e5bb519e26f64e4aec94ad40bfcb052f2de3d
Darren <59044608+darrenleeyx@users.noreply.github.com>
2024-11-06 23:17:13 +0800

    bug fix: incorrect function name, ensure dir exist

--- __settings__/settings_manager.py
+++ __settings__/settings_manager.py
@@ -7,25 +7,34 @@ class SettingsManager:
         self.key_bindings = self.settings['key_bindings']
 
     def get_settings(self):
-        settings_file_path = os.path.join(os.path.dirname(__file__), 'settings.yaml')
+        settings_dir = '__settings__'
+        settings_file_path = f'{settings_dir}\settings.yaml'
 
         if not os.path.exists(settings_file_path):
+            if not os.path.exists(settings_dir):
+                os.makedirs(settings_dir, exist_ok=True)
             self.create_default_settings_file(settings_file_path)
 
         with open(settings_file_path, 'r') as settings_file:
             return yaml.safe_load(settings_file)
 
-    def create_default_settings(self, file_path):
+    def create_default_settings_file(self, file_path):
         default_settings = yaml.safe_load("""
         key_bindings:
-          - name: copy
-            bind_from: ['f8']
-            bind_to: ['ctrl', 'c']
-            enabled: true
-          - name: paste
-            bind_from: ['f9']
-            bind_to: ['ctrl', 'v']
-            enabled: true
+        - bind_from:
+        - f8
+        bind_to:
+        - ctrl
+        - c
+        enabled: true
+        name: copy
+        - bind_from:
+        - f9
+        bind_to:
+        - ctrl
+        - v
+        enabled: true
+        name: paste
         """)
         with open(file_path, 'w') as settings_file:
             yaml.safe_dump(default_settings, settings_file)
